9113d1a796e69f8696f00f61f4514bcdcbeebe4c
Chetan Kothari <chetan.cmk@gmail.com>
2013-04-29 10:44:49 +0530

    Removing layers and adding new ones on refresh.

--- web/war/static/js/locationsDisplay.js
+++ web/war/static/js/locationsDisplay.js
@@ -1,79 +1,93 @@
 function toDegree(radian) {
-	return (radian * 180) / Math.PI;
+  return (radian * 180) / Math.PI;
 }
-function updateMap(map) {
-	if (map.lineLayer && map.myMarkers && map.sharedMarkers) {
-		map.removeLayer(lineLayer);
-		map.removeLayer(myMarkers);
-		map.removeLayer(sharedMarkers);
-	}
-	function onUpdate(serverLocations, status) {
-		var myMarkers = new OpenLayers.Layer.Markers("My Location Markers");
-		map.addLayer(myMarkers);
 
-		var size = new OpenLayers.Size(21, 25);
-		var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
-		var icon = new OpenLayers.Icon('/static/img/marker.png', size, offset);
+function MapView() {
+  var map = new OpenLayers.Map('map');
+  var layer = new OpenLayers.Layer.OSM("Simple OSM Map");
+  map.addLayer(layer);
+  map.addControl(new OpenLayers.Control.LayerSwitcher());
+  map.setCenter(new OpenLayers.LonLat(0, 0), 000000000);
+  var lineLayer, myMarkers, sharedMarkers;
+  this.updateMap = function() {
+    function onUpdate(serverLocations, status) {
+      if (lineLayer && myMarkers && sharedMarkers) {
+        map.removeLayer(lineLayer);
+        map.removeLayer(myMarkers);
+        map.removeLayer(sharedMarkers);
+      }
+      myMarkers = new OpenLayers.Layer.Markers("My Location Markers");
+      map.addLayer(myMarkers);
 
-		var lineLayer = new OpenLayers.Layer.Vector("Line Layer");
-		map.addLayer(lineLayer);
-		var points = new Array();
-		var obj = serverLocations;
-		var i = 0;
-		while (i < Number(obj.locations.length)) {
-			var myPoints = new OpenLayers.Geometry.Point(
-					toDegree(obj.locations[i].long),
-					toDegree(obj.locations[i].lat));
-			points.push(myPoints);
-			myMarkers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(
-					toDegree(obj.locations[i].long),
-					toDegree(obj.locations[i].lat)).transform(
-					new OpenLayers.Projection("EPSG:4326"),
-					new OpenLayers.Projection("EPSG:900913")), icon.clone()));
-			i++;
-		}
+      var size = new OpenLayers.Size(21, 25);
+      var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
+      var icon = new OpenLayers.Icon('/static/img/marker.png', size, offset);
 
-		var sharedMarkers = new OpenLayers.Layer.Markers(
-				"Shared Location Markers");
-		map.addLayer(sharedMarkers);
-		var sharedLocations = obj.sharedLocations;
-		$.each(sharedLocations, function(key, value) {
-			sharedMarkers.addMarker(new OpenLayers.Marker(
-					new OpenLayers.LonLat(toDegree(value.long),
-							toDegree(value.lat)).transform(
-							new OpenLayers.Projection("EPSG:4326"),
-							new OpenLayers.Projection("EPSG:900913")), icon.clone()));
-		});
+      lineLayer = new OpenLayers.Layer.Vector("Line Layer");
+      map.addLayer(lineLayer);
+      var points = new Array();
+      var obj = serverLocations;
+      var i = 0;
+      while (i < Number(obj.locations.length)) {
+        var myPoints = new OpenLayers.Geometry.Point(
+            toDegree(obj.locations[i].long),
+            toDegree(obj.locations[i].lat)
+        );
+        points.push(myPoints);
+        myMarkers.addMarker(new OpenLayers.Marker(
+          new OpenLayers.LonLat(
+            toDegree(obj.locations[i].long), 
+            toDegree(obj.locations[i].lat)).transform(
+              new OpenLayers.Projection("EPSG:4326"), 
+              new OpenLayers.Projection("EPSG:900913")
+            ),
+            icon.clone()
+          )
+        );
+        i++;
+      }
 
-		var line = new OpenLayers.Geometry.LineString(points).transform(
-				new OpenLayers.Projection("EPSG:4326"),
-				new OpenLayers.Projection("EPSG:900913"));
+      sharedMarkers = new OpenLayers.Layer.Markers("Shared Location Markers");
+      map.addLayer(sharedMarkers);
+      var sharedLocations = obj.sharedLocations;
+      $.each(sharedLocations, function(key, value) {
+        sharedMarkers.addMarker(new OpenLayers.Marker(
+          new OpenLayers.LonLat(
+            toDegree(value.long),
+            toDegree(value.lat)).transform(
+              new OpenLayers.Projection("EPSG:4326"),
+              new OpenLayers.Projection("EPSG:900913")
+            ), 
+            icon.clone()
+          )
+        );
+      });
 
-		var style = {
-			strokeColor : '#0000ff',
-			strokeOpacity : 0.5,
-			strokeWidth : 5
-		};
+      var line = new OpenLayers.Geometry.LineString(points).transform(
+          new OpenLayers.Projection("EPSG:4326"), 
+          new OpenLayers.Projection("EPSG:900913")
+        );
 
-		var lineFeature = new OpenLayers.Feature.Vector(line, null, style);
+      var style = {
+        strokeColor : '#0000ff',
+        strokeOpacity : 0.5,
+        strokeWidth : 5
+      };
 
-		lineLayer.addFeatures([ lineFeature ]);
-		map.zoomToExtent(myMarkers.getDataExtent() || sharedMarkers.getDataExtent());
-	}
-	$.ajax({
-		url : "http://localhost:8888/api/json/retrieve",
-		success : onUpdate
-	});
+      var lineFeature = new OpenLayers.Feature.Vector(line, null, style);
+
+      lineLayer.addFeatures([ lineFeature ]);
+      map.zoomToExtent(myMarkers.getDataExtent() || sharedMarkers.getDataExtent());
+    }
+    $.ajax({
+      url : "http://localhost:8888/api/json/retrieve",
+      success : onUpdate
+    });
+  }
 }
 
 $(window).load(function() {
-	OpenLayers.ImgPath = "/static/img/";
-	var map = new OpenLayers.Map('map');
-	var layer = new OpenLayers.Layer.OSM("Simple OSM Map");
-	map.addLayer(layer);
-	map.addControl(new OpenLayers.Control.LayerSwitcher());
-	map.setCenter(new OpenLayers.LonLat(0, 0), 000000000);
-	$("#mapUpdate").click(function() {
-		updateMap(map);
-	});
+  OpenLayers.ImgPath = "/static/img/";
+  var mapView = new MapView;
+  $("#mapUpdate").click(mapView.updateMap);
 });
